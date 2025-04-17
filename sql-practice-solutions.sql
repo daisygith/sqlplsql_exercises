533b61522e6c9cb4c6235ebd12c9ad8acca12224
Sylwia Dudek <117667906+daisygith@users.noreply.github.com>
2025-04-17 19:54:41 +0200

    Update sql-practice-solutions.sql

--- sql-practice-solutions.sql
+++ sql-practice-solutions.sql
@@ -373,4 +373,67 @@ order by doctor_id, selected_year;
 
 Database NORTHWIND
 
-54. 
+54. Show the category_name and description from the categories table sorted by category_name.
+
+select category_name, description from categories;
+
+55. Show all the contact_name, address, city of all customers which are not from 'Germany', 'Mexico', 'Spain'
+
+select contact_name, address, city from customers where country not in ('Germany', 'Mexico', 'Spain');
+
+56. Show order_date, shipped_date, customer_id, Freight of all orders placed on 2018 Feb 26
+
+select order_date, shipped_date, customer_id, freight from orders where order_date = '2018-02-26';
+
+57. Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date
+
+select employee_id, order_id, customer_id, required_date, shipped_date from orders where shipped_date > required_date
+
+
+58. Show all the even numbered Order_id from the orders table
+
+select order_id from orders where order_id%2=0;
+
+59. Show the city, company_name, contact_name of all customers from cities which contains the letter 'L' in the city name, sorted by contact_name
+
+select city, company_name, contact_name from customers where city like '%l%' order by contact_name;
+
+60. Show the company_name, contact_name, fax number of all customers that has a fax number. (not null)
+
+select company_name, contact_name, fax from customers where fax is not null;
+
+61. Show the first_name, last_name. hire_date of the most recently hired employee.
+
+select first_name, last_name, max(hire_date) from employees;
+
+62. Show the average unit price rounded to 2 decimal places, the total units in stock, total discontinued products from the products table.
+
+select round(avg(unit_price),2) as average_price, sum(units_in_stock) as total_stock, sum(discontinued) as total_discontinued from products
+
+63. Show the ProductName, CompanyName, CategoryName from the products, suppliers, and categories table
+
+select p.product_name, s.company_name, cat.category_name from categories cat 
+join products p on cat.category_id = p.category_id
+join suppliers s on p.supplier_id = s.supplier_id
+
+64. Show the category_name and the average product unit price for each category rounded to 2 decimal places.
+
+select cat.category_name, round(avg(p.unit_price),2) from categories cat 
+join products p on cat.category_id = p.category_id
+group by cat.category_name
+
+65. Show the city, company_name, contact_name from the customers and suppliers table merged together.
+Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
+
+select City, company_name, contact_name, 'customers' as relationship 
+from customers
+union
+select city, company_name, contact_name, 'suppliers'
+from suppliers
+
+66. Show the total amount of orders for each year/month.
+
+select year(order_date) as order_year, month(order_date) as order_month, count(order_id) as no_of_orders from orders
+group by order_year,order_month
+
+67.
